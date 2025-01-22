a8436fba67c7be1e6fc0139d9f8c16062c427157
li-moo <tpfladl1111@gmail.com>
2025-01-23 01:36:03 +0900

    location maeker fix

--- src/components/pages/location/YoloMap.jsx
+++ src/components/pages/location/YoloMap.jsx
@@ -24,12 +24,12 @@ const YoloMap = () => {
 
         // const imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png';
         const imageSrc = `${process.env.PUBLIC_URL}/yolomarker.png`;
-        const imageSize = new window.kakao.maps.Size(63, 65); // 마커이미지의 크기입니다
+        const imageSize = new window.kakao.maps.Size(40, 45); // 마커이미지의 크기입니다
         const imageOption = { offset: new window.kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
         // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
         const markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-        const markerPosition = new window.kakao.maps.LatLng(35.86723, 129.27306); // 마커가 표시될 위치입니다
+        const markerPosition = new window.kakao.maps.LatLng(35.86693, 129.27327); // 마커가 표시될 위치입니다
 
         // 마커를 생성합니다
         const marker = new window.kakao.maps.Marker({
